6532183c4e53420cb6bb99b71399506db46b83ec
Myles Adams <mylesa1@student.unimelb.edu.au>
2018-03-16 13:00:16 +1100

    Further Changes

--- ProjectA.py
+++ ProjectA.py
@@ -2,70 +2,76 @@ class main():
 
     black = []
     white = []
+    totalPositions = []
 
     def findPieces(input, black, white):
 
         for i in range(8):
             for j in range(8):
-
                 if input[i][j] == "O":
                         white.append([i,j])
 
                 if input[i][j] == "@":
                         black.append([i,j])
 
+        if input[8] == "Moves":
+            print("moves")
 
-    def possibleMoves(player, board):
 
-        totalPositions = []
+    def possibleMoves(player, board, totalPositions):
+
+        totalmoves = 0
 
         for i in range(len(player)):
 
             positions = []
-            pos = player[i]
-
-            y = pos[0]
-            x = pos[1]
+            y = player[i][0]
+            x = player[i][1]
 
             if (x>0):# check left
                 if (board[y][x-1] == "-"):
                     positions.append([y, x-1])
+                    totalmoves += 1
                 elif x>1 and (board[y][x-1] == "@" or board[y][x-1] == "O"):
-
                     if (board[y][x-2] == "-"):
-
                         positions.append([y, x-2])
+                        totalmoves += 1
 
             # check up
             if (y>0):
                 if (board[y-1][x] == "-"):
                     positions.append([y-1, x])
+                    totalmoves += 1
                 elif y>1 and (board[y-1][x] == "@" or board[y+1][x] == "O"):
                     if (board[y-2][x] == "-"):
                         positions.append([y-2, x])
+                        totalmoves += 1
 
             # check right
             if (x<7):
 
                 if (board[y][x+1] == "-"):
                     positions.append([y, x+1])
-
-                if x<6 and (board[y][x+1] == "@" or board[y][x+1] == "O"):
-
+                    totalmoves += 1
+                elif x<6 and (board[y][x+1] == "@" or board[y][x+1] == "O"):
                     if (board[y][x+2] == "-"):
                         positions.append([y, x+2])
+                        totalmoves += 1
 
             # check down
             if (y<7):
                 if (board[y+1][x] == "-"):
                     positions.append([y+1, x])
+                    totalmoves += 1
                 elif y<6 and (board[y+1][x] == "@" or board[y+1][x] == "O"):
                     if (board[y+2][x] == "-"):
                         positions.append([y+2, x])
+                        totalmoves += 1
 
             totalPositions.append(positions)
 
-        return totalPositions
+        print(totalPositions)
+        return totalmoves
 
     empty = [['X', '-', '-', '-', '-', '-', '-', 'X'],
             ['-', '-', '-', '-', '-', '-', '-', '-'],
@@ -81,17 +87,17 @@ class main():
     test1 = [['X', '-', '@', 'O', '-', '-', '-', 'X'],
             ['-', '-', '-', '@', '-', '-', '-', '@'],
             ['-', '-', '-', '-', '-', '-', '-', '-'],
-            ['0', '-', '-', '-', '0', '-', '-', '-'],
+            ['O', '-', '-', '-', 'O', '-', '-', '-'],
             ['-', '-', '-', '-', '-', '-', '-', '-'],
-            ['-', '-', '0', '-', '-', '-', '-', '-'],
-            ['@', '-', '@', 'O', '@', '-', '-', '-'],
-            ['X', '-', '-', '-', '-', '-', '-', 'X']]
-
+            ['-', '-', '-', '-', '-', '-', '-', '-'],
+            ['@', '-', '@', '-', '@', '-', '-', '-'],
+            ['X', '-', '-', '-', '-', '-', '-', 'X'],
+            "Moves"]
 
     findPieces(test1, black, white)
 
-    movesB = possibleMoves(black, test1)
-    movesW = possibleMoves(white, test1)
+    movesB = possibleMoves(black, test1, totalPositions)
+    movesW = possibleMoves(white, test1, totalPositions)
 
-    print("black \n",movesB)
-    print("\n white \n",movesW)
+    print(movesB)
+    print(movesW)
